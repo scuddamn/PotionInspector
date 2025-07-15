31902f5f387b292f2719d92aaa8121a39653d4a1
scuddamn <aescudder@gmail.com>
2025-07-15 16:45:21 +0200

    DOTween saved my life

---
+++ Assets/Scripts/BookHover.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public class BookHover : MonoBehaviour
+{
+    private BookManager bookManager;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bookManager = FindFirstObjectByType<BookManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        
+        bookManager.MoveToHover();
+        print("hovering");
+    }
+
+    private void OnMouseExit()
+    { 
+        if (bookManager.IsOpen()) return;
+        
+        bookManager.MoveOffscreen();
+        print("leaving hover");
+    }
+
+    private void OnMouseDown()
+    {
+        if (!bookManager.IsOpen())
+        {
+            bookManager.MoveToOpen();
+        }
+        
+    }
+}
--- Assets/Scripts/BookManager.cs
+++ Assets/Scripts/BookManager.cs
@@ -1,62 +1,59 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BookManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> snapPositions;
     [SerializeField] private float moveSpeed = 1.0f;
     private GameObject bookCanvas;
-    private bool isHover = false;
-    private GameObject guidebook;
-    private Vector3 bookPosition;
-    private Vector3 targetPosition;
-    
+    private bool isOpen;
+
+    public bool IsOpen()
+    {
+        return isOpen;
+    }
+
     private void Awake()
     {
         bookCanvas = GameObject.FindWithTag("BookUI");
-        guidebook = GameObject.FindWithTag("Guidebook");
-        bookPosition = new Vector3(guidebook.transform.position.x, guidebook.transform.position.y);
-        
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bookPosition = snapPositions[0].transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        
-        bookPosition = Vector3.Lerp(bookPosition, targetPosition, moveSpeed * Time.deltaTime);
-        
+    {
+
+    }
+
+    public void MoveToOpen()
+    {
+            transform.DOMove(snapPositions[2].transform.position, moveSpeed);
+            bookCanvas.SetActive(true);
+            isOpen = true;
+    }
+
+    public void MoveOffscreen()
+    {
+        transform.DOMove(snapPositions[0].transform.position, moveSpeed);
+    }
+
+    public void MoveToHover()
+    {
+        transform.DOMove(snapPositions[1].transform.position, moveSpeed);
+    }
+
+    public void CloseBook()
+    {
+        bookCanvas.SetActive(false);
+        isOpen = false;
     }
     
-    private void OnMouseEnter()
-    {
-        isHover = true;
-        targetPosition = snapPositions[1].transform.position;
-    }
-
-    private void OnMouseExit()
-    {
-        isHover = false;
-        targetPosition = snapPositions[0].transform.position;
-    }
-
-    private void OnMouseDown()
-    {
-        if (isHover)
-        {
-            bookPosition = Vector3.Lerp(bookPosition, snapPositions[2].transform.position, moveSpeed * Time.deltaTime);
-            bookCanvas.SetActive(true);
-        }
-    }
-
-    public void BookReturn()
-    {
-        bookPosition = Vector3.Lerp(bookPosition, snapPositions[0].transform.position, moveSpeed * Time.deltaTime);
-    }
+    
 }

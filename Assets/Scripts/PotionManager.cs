130d02f18da814aa80d4a1b549c41de7393a215d
scuddamn <aescudder@gmail.com>
2025-07-22 17:34:57 +0200

    potion slides onto screen with dialogue box + visual inspection added

--- Assets/Scripts/PotionManager.cs
+++ Assets/Scripts/PotionManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,20 +10,22 @@ public class PotionManager : MonoBehaviour
     [SerializeField] private List<ScriptablePotions> potionOptions;
     private ScriptablePotions currentPotion;
 
-    [SerializeField] private Transform aromaDisplay;
+    [SerializeField] private GameObject aromaDisplay;
     [SerializeField] private TextMeshProUGUI dialogueBox;
     [SerializeField] private TMP_Text nametag;
-    [SerializeField] private GameObject inspectionIcon;
+    [SerializeField] private GameObject potionInspectionIcon;
+    [SerializeField] private TextMeshProUGUI inspectionText;
     
     [SerializeField] private Transform potionSnapOffscreen;
     [SerializeField] private Transform potionSnapDesk;
-    
+    [SerializeField] private float moveSpeed = 1f;
     private AromaType aromaManager;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentPotion = potionOptions[0];
+        currentPotion = potionOptions[0]; //TEMP
+        transform.position = potionSnapOffscreen.position;
         aromaManager = FindFirstObjectByType<AromaType>();
     }
 
@@ -35,14 +38,33 @@ public class PotionManager : MonoBehaviour
     public void OnNewCustomer()
     {
         GetComponentInChildren<Image>().sprite = currentPotion.GetPotionSprite();
-        inspectionIcon.GetComponent<Image>().sprite = currentPotion.GetPotionSprite();
+        potionInspectionIcon.GetComponent<Image>().sprite = currentPotion.GetPotionSprite();
         nametag.text = $"{currentPotion.GetPotionCreator()}";
         dialogueBox.text = $"This is my {currentPotion.GetPotionName()}";
+        aromaDisplay.SetActive(false);
     }
-    
+
+    public void PotionGiven()
+    {
+        transform.DOMove(potionSnapDesk.position, moveSpeed);
+    }
+
+    public void PotionTaken()
+    {
+        transform.DOMove(potionSnapOffscreen.position, moveSpeed);
+    }
+
+    public void InspectPotion() //colorblind-friendly inspection method that describes the potion color
+    {
+        aromaDisplay.SetActive(false);
+        inspectionText.text = $"{currentPotion.GetPotionType()} liquid swirls inside this bottle.";
+    }
 
     public void DisplayAroma()
     {
+        inspectionText.text = "";
+        aromaDisplay.SetActive(true);
+        
         List<AromaType.AromaOptions> aromas = currentPotion.GetAromas();
         
         List<Sprite> aromaIcons = new List<Sprite>();
@@ -52,7 +74,7 @@ public class PotionManager : MonoBehaviour
         }
         
         List<Transform> aromaDisplays = new List<Transform>();
-        foreach (Transform child in aromaDisplay)
+        foreach (Transform child in aromaDisplay.transform)
         {
             aromaDisplays.Add(child);
         }

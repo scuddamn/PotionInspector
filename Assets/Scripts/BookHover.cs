1a32fed7e64d6747c93cbc50fcb7970ec8d891dd
scuddamn <aescudder@gmail.com>
2025-07-25 13:31:00 +0200

    book will no longer appear if tools are on the desk
    - to avoid tools becoming trapped beneath book

--- Assets/Scripts/BookHover.cs
+++ Assets/Scripts/BookHover.cs
@@ -1,24 +1,35 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class BookHover : MonoBehaviour
 {
+    [SerializeField] private GameObject toolHolder;
     private BookManager bookManager;
-    
+    private Collider2D hoverArea;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        hoverArea = GetComponent<Collider2D>();
         bookManager = FindFirstObjectByType<BookManager>(); //get reference to the script on the book item
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (toolHolder.transform.childCount > 0)
+        {
+            hoverArea.enabled = false;
+        } else if (toolHolder.transform.childCount <= 0)
+        {
+            hoverArea.enabled = true;
+        }
     }
 
     private void OnMouseEnter()
     { //while hovering over area
+        
         if (!bookManager.IsOpen())
         {
             bookManager.MoveToHover();
@@ -28,6 +39,7 @@ public class BookHover : MonoBehaviour
 
     private void OnMouseExit()
     { //when stopped hovering over area
+        
         if (!bookManager.IsOpen())
         {
             bookManager.MoveOffscreen();

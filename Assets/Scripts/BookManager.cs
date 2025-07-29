f9ff3d49b1543587ad67fb259b6d83cf3bbf15ba
scuddamn <aescudder@gmail.com>
2025-07-29 17:28:55 +0200

    worked on new guidebook animations
    buttons that should turn pages do nothing....unclear why

--- Assets/Scripts/BookManager.cs
+++ Assets/Scripts/BookManager.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 
 public class BookManager : MonoBehaviour
 {
-    private static readonly int FadeOut = Animator.StringToHash("fadeOut");
+    private static readonly int Open = Animator.StringToHash("open");
     private static readonly int FadeIn = Animator.StringToHash("fadeIn");
-    
+    private static readonly int FadeOut = Animator.StringToHash("fadeOut");
+    private static readonly int TurnBack = Animator.StringToHash("turnBack");
+    private static readonly int TurnNext = Animator.StringToHash("turnNext");
+    private static readonly int Close = Animator.StringToHash("close");
+
     [Header("Book Hover + Movement")]
     [SerializeField] List<GameObject> snapPositions;
     [SerializeField] private float moveSpeed = 1.0f;
@@ -18,9 +22,10 @@ public class BookManager : MonoBehaviour
     [SerializeField] private GameObject bookCanvas;
     [SerializeField] private GameObject bookPage;
     [SerializeField] private float textFadeSpeed = 1f;
-    [SerializeField] private TMP_Text titleText;
-    [SerializeField] private TMP_Text bodyText;
+    [SerializeField] private TMP_Text[] pageText;
     [SerializeField] private float pageFadeSpeed = 2f;
+
+    private Animator animator;
     
     private bool isOpen;
 
@@ -31,16 +36,22 @@ public class BookManager : MonoBehaviour
     
     void Start()
     {
-        titleText.alpha = 0;
-        bodyText.alpha = 0; //set initial text alpha value to 0, so the text can fade in upon opening the menu
+        foreach (var text in pageText)
+        {
+            text.alpha = 0;
+        } //set initial text alpha value to 0, so the text can fade in upon opening the menu
+        
+        animator = GetComponentInChildren<Animator>();
     }
     
     public void MoveToOpen() //move the book to the 'activated' position on the desk
     {
             transform.DOMove(snapPositions[2].transform.position, moveSpeed);
+            animator.SetTrigger(Open);
+            transform.DOScale(2.1f, moveSpeed);
             isOpen = true;
-            transform.DOScale(2f, 1f);
-            Invoke(nameof(OpenMenu), 0.5f);
+            Invoke(nameof(OpenMenu), 1f);
+            
     }
 
     void OpenMenu() //opens the book menu
@@ -54,14 +65,20 @@ public class BookManager : MonoBehaviour
 
     void FadeInText() //page text fades in to full alpha
     {
-        bodyText.DOFade(1, textFadeSpeed);
-        titleText.DOFade(1, textFadeSpeed);
+        foreach (var text in pageText)
+        {
+            text.DOFade(1, textFadeSpeed);
+        }
+        
+        
     }
     
     void FadeOutText() //page text fades from full alpha to 0 
     {
-        bodyText.DOFade(0, textFadeSpeed);
-        titleText.DOFade(0, textFadeSpeed);
+        foreach (var text in pageText)
+        {
+            text.DOFade(0, textFadeSpeed);
+        }
     }
     
     public void MoveOffscreen() //moves book offscreen to initial position
@@ -74,8 +91,19 @@ public class BookManager : MonoBehaviour
         transform.DOMove(snapPositions[1].transform.position, moveSpeed);
     }
 
+    public void TurnPageNext()
+    {
+        animator.SetTrigger(TurnNext);
+    }
+
+    public void TurnPageBack()
+    {
+        animator.SetTrigger(TurnBack);
+    }
+
     void CloseBook() //book menu closes
     {
+        animator.SetTrigger(Close);
         transform.DOScale(1f, 1f);
         bookCanvas.SetActive(false);
         isOpen = false;
